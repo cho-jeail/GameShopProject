ba609c85231b268e00bd998eb55c8012e77d0b1c
naosd <naosd@DESKTOP-Q6RCMPE>
2020-11-25 19:29:23 +0900

    userVO 내용 입력, product, couponVO 생성 2020-11-25, 19:29

---
+++ GameShop/src/main/java/com/cws/vo/CouponVO.java
@@ -0,0 +1,50 @@
+package com.cws.vo;
+
+import java.util.Date;
+
+public class CouponVO {
+	private String userId;
+	private String id;
+	private int salePrice;
+	private Date validity;
+	private String name;
+	private String fileUrl;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getSalePrice() {
+		return salePrice;
+	}
+	public void setSalePrice(int salePrice) {
+		this.salePrice = salePrice;
+	}
+	public Date getValidity() {
+		return validity;
+	}
+	public void setValidity(Date validity) {
+		this.validity = validity;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFileUrl() {
+		return fileUrl;
+	}
+	public void setFileUrl(String fileUrl) {
+		this.fileUrl = fileUrl;
+	}
+	
+}
