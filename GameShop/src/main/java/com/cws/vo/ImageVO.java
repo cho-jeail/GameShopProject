eaf0c2c71e3949c63d0f9cbff610c5e2fdf4124e
rlatkddn1009 <rlatkddn1009@naver.com>
2020-11-26 13:52:51 +0900

    VO클래스 생성(Image, NoticeBoard, QABoard, QACommnet, Review, Spec)
    Image, NoticeBoard, QABoard, QACommnet, Review, Spec 생성 완료
    
    2020-11-26, 13:52

---
+++ GameShop/src/main/java/com/cws/vo/ImageVO.java
@@ -0,0 +1,19 @@
+package com.cws.vo;
+
+public class ImageVO {
+	private String id;
+	private String imgUrl;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getImgUrl() {
+		return imgUrl;
+	}
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+}
---
+++ GameShop/src/main/java/com/cws/vo/QAComment.java
@@ -0,0 +1,35 @@
+package com.cws.vo;
+
+import java.util.Date;
+
+public class QAComment {
+	private String id;
+	private String content;
+	private String writer;
+	private Date writeDate;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public Date getWriteDate() {
+		return writeDate;
+	}
+	public void setWriteDate(Date writeDate) {
+		this.writeDate = writeDate;
+	}
+}
